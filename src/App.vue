e0e2d8b4ab8149e297689afa23447e6a05b75ed3
Pierre Trollé <ptrolle@octo.com>
2017-02-15 18:42:50 +0100

    gestion de json épuré

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
     <div id="app">
         <p>La vie a évolué {{ counter }} fois. </p>
-        <p>Hauteur de la nouvelle grille à créer :
-            <input v-model="newHeight" placeholder="insert a height">
-        </p>
-        <p>Largeur de la nouvelle grille à créer :
-            <input v-model="newWidth" placeholder="insert a width">
-        </p>
-        <button class="button-post" v-on:click="postCreate">Post a new grid</button>
-        <button class="button-smart-create" v-on:click="smartCreate">Create intelligent new grid</button>
-        <button class="button-create" v-on:click="create">Create new grid</button>
+        <p>Hauteur de la nouvelle grille à créer :<input v-model="newHeight" placeholder="insert a height"></p>
+        <p>Largeur de la nouvelle grille à créer :<input v-model="newWidth" placeholder="insert a width"></p>
+        <button class="button-create" v-on:click="newCreate">Create intelligent and formatted grid</button>
         <button class="button-refresh" v-on:click="refresh">Refresh</button>
         <button class="button-refresh-automatic" v-on:click="refreshAutomatic">Start Refresh Automatic</button>
         <button class="button-stop" v-on:click="stopRefreshAutomatic">Stop refresh Automatic</button>
         <br>
-        <canvas :width="width" :height="height" style="border:1px solid #BBB;"
-                v-insert-in-canvas="cells">
-        </canvas>
+        <canvas :width="width" :height="height" style="border:1px solid #BBB;" v-insert-in-canvas="cells"></canvas>
     </div>
 </template>
 
@@ -29,8 +21,7 @@
         height: 500,
         newWidth: 100,
         newHeight: 50,
-        grid: 'grid',
-        cells: 100,
+        cells: 'cells',
         counter: 0,
         idInterval: 17432,
         oldId: 17433
@@ -59,67 +50,36 @@
           .get('http://localhost:9292/grids/100')
           .then((response) => {
             let json = response.body
-            this.grid = JSON.parse(json)
-            this.width = this.grid.length * 10
-            this.height = this.grid.height * 10
-            this.cells = this.grid.cells
+            this.cells = JSON.parse(json)
+            this.width = this.cells[this.cells.length - 1]['x'] * 10
+            this.height = this.cells[this.cells.length - 1]['y'] * 10
             return this.cells
           }, () => {
             console.log('le refresh est en echec ')
+          })
+      },
+      newCreate: function () {
+        console.log('création intelligente d une nouvelle grille formattée')
+        this.$http
+          .get('http://localhost:9292/newCreate/100/height/' + this.newHeight + '/width/' + this.newWidth)
+          .then((response) => {
+            let json = response.body
+            this.cells = JSON.parse(json)
+            this.width = this.cells[this.cells.length - 1]['x'] * 10
+            this.height = this.cells[this.cells.length - 1]['y'] * 10
+            return this.cells
+          }, () => {
+            console.log('le create est en echec ')
           })
       },
       refreshAutomatic: function () {
         this.oldId = this.idInterval
         this.idInterval = setInterval(this.refresh, 500)
       },
-      create: function () {
-        console.log('création d une nouvelle grille ')
-        this.$http
-          .get('http://localhost:9292/create')
-          .then((response) => {
-            let json = response.body
-            this.grid = JSON.parse(json)
-            this.width = this.grid.length * 10
-            this.height = this.grid.height * 10
-            this.cells = this.grid.cells
-            return this.cells
-          }, () => {
-            console.log('le create est en echec ')
-          })
-      },
-      smartCreate: function () {
-        console.log('création intelligente d une nouvelle grille ')
-        this.$http
-          .get('http://localhost:9292/create/100/height/' + this.newHeight + '/width/' + this.newWidth)
-          .then((response) => {
-            let json = response.body
-            this.grid = JSON.parse(json)
-            this.width = this.grid.length * 10
-            this.height = this.grid.height * 10
-            this.cells = this.grid.cells
-            return this.cells
-          }, () => {
-            console.log('le smart create est en echec ')
-          })
-      },
       stopRefreshAutomatic: function () {
         console.log('un clic détecté ')
         clearInterval(this.idInterval)
         clearInterval(this.oldId)
-      },
-      postCreate () {
-        this.$http
-          .post('http://localhost:9292/grids', {height: '100', width: '50'})
-          .then((response) => {
-            let json = response.body
-            this.grid = JSON.parse(json)
-            this.width = this.grid.length * 10
-            this.height = this.grid.height * 10
-            this.cells = this.grid.cells
-            return this.cells
-          }, (response) => {
-            console.log('le post est en echec ')
-          })
       }
     },
     directives: {
@@ -130,7 +90,7 @@
         const cells = binding.value
 
         ctx.clearRect(0, 0, width, height)
-        ctx.strokeStyle = 'red'
+        ctx.strokeStyle = 'white'
         ctx.fillStyle = 'black'
         for (let x = 0; x < width / 10; x++) {
           for (let y = 0; y < height / 10; y++) {
@@ -138,12 +98,16 @@
           }
         }
 
+        for (let i = 0; i < cells.length; i++) {
+          if (cells[i]['state'] === 'alive') {
+            let x = cells[i]['x']
+            let y = cells[i]['y']
+            ctx.fillRect(10 * (x - 1), 10 * (y - 1), 10, 10)
+          }
+        }
         for (let x = 1; x <= width / 10; x++) {
           for (let y = 1; y <= height / 10; y++) {
             ctx.font = '30px Arial'
-            if (cells['[' + x + ', ' + y + ']'] === 'alive') {
-              ctx.fillRect(10 * (x - 1), 10 * (y - 1), 10, 10)
-            }
           }
         }
       }
