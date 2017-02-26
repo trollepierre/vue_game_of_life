9eb13c12d0bcdee9ab2fa741aaf2ddc141d8ee1a
Pierre Trollé <ptrolle@octo.com>
2017-02-26 21:10:36 +0100

    add test on App.vue

--- test/unit/specs/Grid.spec.js
+++ test/unit/specs/Grid.spec.js
@@ -1,20 +1,26 @@
 import Vue from "vue";
 import Grid from "src/components/Grid";
 
-function getInnerHtml(Grid, propsData) {
+function constructGridWithProps(Grid, propsData) {
   const Ctor = Vue.extend(Grid)
-  const vm = new Ctor({propsData}).$mount()
-  return vm.$el.innerHTML
+  return new Ctor({propsData}).$mount();
 }
 
 describe('Grid.vue', () => {
   it('should render canvas with expected width and height', () => {
     const expectedHeight = '150'
     const expectedWidth = '234'
-    expect(getInnerHtml(Grid, {
+    const propsData = {
       height: expectedHeight,
       width: expectedWidth,
       cells: {}
-    })).to.equal('<canvas width="' + expectedWidth + '" height="' + expectedHeight + '" style="border: 1px solid rgb(187, 187, 187);"></canvas>')
+    }
+    const vm = constructGridWithProps(Grid, propsData);
+
+    expect(vm.$el.innerHTML).to.equal('<canvas width="' + expectedWidth + '" height="' + expectedHeight + '" style="border: 1px solid rgb(187, 187, 187);"></canvas>')
   })
+
+  xit('should call function insertInCanvas', () => {
+  })
+
 })
