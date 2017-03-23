403817b60bf979fcacae5247ec7e1318e0604658
Pierre Trollé <ptrolle@octo.com>
2017-03-23 19:02:06 +0100

    spec app give its props to grid

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -63,14 +63,51 @@ describe('App.vue', () => {
       it('should contain a creation inside bandeau', () => {
         expect(bandeau.querySelector('#creation')).not.to.be.empty
       })
+
+      xit('should only react on click', () => {
+
+      })
     })
 
-    it('should contain a comandContainer inside bandeau', () => {
-      expect(bandeau.querySelector('#commandContainer')).not.to.be.empty
+    describe('command container', function () {
+      it('should contain a comandContainer inside bandeau', () => {
+        expect(bandeau.querySelector('#commandContainer')).not.to.be.empty
+      })
+
+      xit('should react on refresh click', () => {
+
+      })
+
+      xit('should react on  auto refresh click', () => {
+
+      })
+
+      xit('should react on  stop refresh click', () => {
+
+      })
     })
   })
 
-  it('should contain a grid', () => {
-    expect(vm.$el.querySelector('#grid')).not.to.be.empty
+  describe('grid', function () {
+    it('should contain a grid', () => {
+      expect(vm.$el.querySelector('#grid')).not.to.be.empty
+    })
+
+    it('should get prop cells, width, height', () => {
+      let expectedHeight = '151'
+      let expectedWidth = '235'
+      let cells = {}
+      let data = {
+        data: {
+          height: expectedHeight,
+          width: expectedWidth,
+          cells: cells
+        }
+      }
+      vm = constructAppWithProps(App, data)
+      let canvas = vm.$el.querySelector('canvas')
+      expect(canvas.getAttribute('width')).to.equal(expectedWidth)
+      expect(canvas.getAttribute('height')).to.equal(expectedHeight)
+    })
   })
 })
